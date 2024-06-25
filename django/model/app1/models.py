0376d711d6304892a058bb99fddb9d17c7a6fc26
DongWan <dlehddhks125@naver.com>
2024-06-25 18:00:19 +0900

    2024-06-25

---
+++ django/model/app1/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = "person"
+
+    # makemigrations 대상 아님
+    def __str__(self) -> str:
+        return self.first_name + " " + self.last_name
--- django/myapp1/todo/models.py
+++ django/myapp1/todo/models.py
@@ -5,9 +5,11 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
+    # auto_now_add : 새글등록 시 자동으로 날짜 추가
     created_at = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
     important = models.BooleanField(default=False)
 
+    # 자바에 tostring
     def __str__(self) -> str:
         return self.title
